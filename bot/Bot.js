613c4ab1deeca63ac1a7d34e341f9edf751d5df1
Glitch (hello-express) <none>
2020-05-25 10:41:24 +0000

    💳🎾 Checkpoint ./bot/Bot.js:16220712/1068

--- bot/Bot.js
+++ bot/Bot.js
@@ -62,7 +62,7 @@ bot.on("voiceStateUpdate", (oldState, newState) => {
         console.log(err);
     }
 });
-
+//var havardID = 132193704860450817;
 bot.on("message", msg => {
   if(msg.author.bot){
     return;
@@ -75,11 +75,13 @@ bot.on("message", msg => {
     } else if (msg.content.includes("good bot")) {
         msg.reply(
             "Tusen takk, vi hos HenckeBot Inc Ltd. vet at vår bot er mye bedre enn TrashBOT, vennligst skriv et brev til denne søppelboten og gi 1-star review på google");
-    }else if(msg.content.includes("hmm")){
+    }else if(msg.content.includes("hmm") && msg.author.id != '132193704860450817'){
        msg.channel.send({files: ["https://media.tenor.com/images/69983541b409be168812dfd95be4bbf2/tenor.gif"]});
-    }else if(msg.content.includes("reee")){
+    }else if(msg.content.includes("reee") && msg.author.id != '132193704860450817'){
        msg.channel.send({files: ["https://cdn.glitch.com/2c6c8596-d523-4520-a5a7-8caa66a05edf%2F1b72da8a-c1d0-48e4-92df-f8b35fe2fb40.bilde.png?v=1589880897535"]});
-    }else if (msg.content.includes("nuddel")) {
+    }else if (msg.author.id = "132193704860450817" && msg.content.includes("https://tenor.com")){
+                    msg.delete().catch(O_o => { });
+    } else if(msg.content.includes("nuddel")) {
                 url = "https://cdn.glitch.com/05aa1396-7f5d-45a3-ab76-baf2815a144a%2Fskididdel.mp3?v=1589665443245",
                 sound(url, msg.member.voice.channel, msg);
     }else if (msg.content.includes("F") && msg.content.length < 2) {
