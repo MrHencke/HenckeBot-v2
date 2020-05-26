94afa1869f082845c6058e08de7c5d9cb2fc9d6a
Glitch (hello-express) <none>
2020-05-26 14:18:45 +0000

    🤗🍂 Checkpoint

--- bot/Bot.js
+++ bot/Bot.js
@@ -27,7 +27,7 @@ function sound(url, voiceChannel, msg) {
   try{
     if(voiceChannel != null){
     voiceChannel.join().then(connection => {
-        const dispatcher = connection.play(url, { volume: 0.2});
+        const dispatcher = connection.play(url, { volume: 0.3});
         dispatcher.on("finish", end => voiceChannel.leave());
     }).catch(err => console.log(err));
     }else msg.channel.send("Vet du forskjellen på voice og tekst? Gå inn i en voicechannel da din nepe");
