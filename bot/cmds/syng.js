25ce35398150e86b7d2ebec3f19bf2610e206906
Glitch (hello-express) <none>
2020-06-07 11:49:03 +0000

    🌞🚘 Checkpoint ./bot/cmds/syng.js:16220712/1341

--- bot/cmds/syng.js
+++ bot/cmds/syng.js
@@ -1,5 +1,3 @@
-var path = require("path")
-const sound = require(path.join(__dirname, '..', '..', 'res/sound.js'));
 const lyrics = require("node-lyrics-api");
 const discordTTS=require("discord-tts");
 const discord = require("discord.js");
@@ -11,15 +9,32 @@ module.exports = {
     execute(bot, msg, args) {
       var Title = args.join(' ');
         lyrics(Title).then(result => {
-        if(result.content[0].lyrics<199){
+        if(result.content[0].lyrics.length>199){
           var resultat = result.content[0].lyrics
-          console.log(resultat);
-        //var resultata = resultat.match(/[\s\S]{199}/);
+        var resultata = resultat.match(/[\s\S]{199}/);
+        for(var i = 0; i < resultata.length; i){
+        
+        sound(discordTTS.getVoiceStream(resultata[i]),msg.member.voice.channel, msg)
+          }
+        }else {
+        sound(discordTTS.getVoiceStream(result.content[0].lyrics),msg.member.voice.channel, msg)
         }
-        sound(discordTTS.getVoiceStream("test123"),msg.member.voice.channel, msg)
 }).catch(error => {
     msg.channel.send("Fant ikke noe sang å synge :c");
 });  
     },
 };
+
+
+function sound(url, voiceChannel, msg) {
+  try{
+    if(voiceChannel != null){
+    voiceChannel.join().then(connection => {
+        const dispatcher = connection.play(url, { volume: 0.4});
+        dispatcher.on("finish", end => );
+    }).catch(err => console.log(err));
+    }else msg.channel.send("Vet du forskjellen på voice og tekst? Gå inn i en voicechannel da din nepe");
+    }catch(err){
+      //console.log(err)
+    }}
 
