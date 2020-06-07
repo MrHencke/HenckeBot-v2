f1c498f059f12b0346d261678ad492579c7b19b2
Glitch (hello-express) <none>
2020-06-07 12:00:04 +0000

    🐉🏬 Checkpoint ./bot/cmds/syng.js:16220712/1837

--- bot/cmds/syng.js
+++ bot/cmds/syng.js
@@ -10,15 +10,13 @@ module.exports = {
       var Title = args.join(' ');
         lyrics(Title).then(result => {
         if(result.content[0].lyrics.length>199){
+          var i = 0;
           var resultat = result.content[0].lyrics
-        var resultata = resultat.match(/[\s\S]{199}/);
-        for(var i = 0; i < resultata.length; i){
-        
-        sound(discordTTS.getVoiceStream(resultata[i]),msg.member.voice.channel, msg)
-          }
-        }else {
-        sound(discordTTS.getVoiceStream(result.content[0].lyrics),msg.member.voice.channel, msg)
-        }
+          var resultata = resultat.match(/[\s\S]{199}/);
+        console.log(resultata[i])
+        sound(discordTTS.getVoiceStream(resultata[i]), msg.member.voice.channel, msg, resultata)
+          
+        }    
 }).catch(error => {
     msg.channel.send("Fant ikke noe sang å synge :c");
 });  
@@ -26,12 +24,13 @@ module.exports = {
 };
 
 
-function sound(url, voiceChannel, msg) {
+function sound(url, voiceChannel, msg, resultata) {
   try{
-    if(voiceChannel != null){
+    var i = 0;
+    if(voiceChannel != null && i < resultata.length){
     voiceChannel.join().then(connection => {
         const dispatcher = connection.play(url, { volume: 0.4});
-        dispatcher.on("finish", end => );
+        dispatcher.on("finish", end => sound(discordTTS.getVoiceStream(url[i++]),msg.member.voice.channel, msg));
     }).catch(err => console.log(err));
     }else msg.channel.send("Vet du forskjellen på voice og tekst? Gå inn i en voicechannel da din nepe");
     }catch(err){
