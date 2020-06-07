3df63376d343406dee159e501f989c2424f7707d
Glitch (hello-express) <none>
2020-06-07 10:50:03 +0000

    ☄️🕴 Checkpoint ./bot/cmds/wrong.js:16220712/5 ./bot/cmds/syng.js:16220712/5

--- bot/cmds/wrong.js
+++ bot/cmds/wrong.js
@@ -7,5 +7,6 @@ module.exports = {
     aliases: [''],
     execute(bot, msg, args) {
       var url = "https://cdn.glitch.com/05aa1396-7f5d-45a3-ab76-baf2815a144a%2Fwrong.mp3?v=1589662557794";
-                sound(url, msg.member.voice.channel, msg);    },
+                sound(url, msg.member.voice.channel, msg);    
+    },
 };
