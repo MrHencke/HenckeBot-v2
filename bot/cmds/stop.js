7135c98576c3ea03112d496a4037d1d48091b734
Glitch (hello-express) <none>
2020-06-08 23:17:57 +0000

    👩🚓 Checkpoint ./bot/cmds/stop.js:16220712/1039

---
+++ bot/cmds/stop.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'stop',
+    description: '',
+    aliases: [''],
+    execute(bot, msg, args) {    
+      try{
+    if(voiceChannel != null){
+    voiceChannel.join().then(connection => {
+        const dispatcher = connection.play(url, { volume: 0.4});
+        dispatcher.on("finish", end => voiceChannel.leave());
+    }).catch(err => console.log(err));
+    }else msg.channel.send("Vet du forskjellen på voice og tekst? Gå inn i en voicechannel da din nepe");
+    }catch(err){
+    },
+};
