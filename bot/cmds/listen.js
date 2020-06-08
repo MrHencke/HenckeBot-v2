a56ccc4f7ff1ff0e12f644b0529c04e1d27edc43
Glitch (hello-express) <none>
2020-06-08 23:41:17 +0000

    🌆🏄 Checkpoint ./bot/cmds/listen.js:16220712/2671

--- bot/cmds/listen.js
+++ bot/cmds/listen.js
@@ -1,12 +1,53 @@
-var path = require("path")
-const sound = require(path.join(__dirname, '..', '..', 'res/sound.js'));
-
 module.exports = {
   name: 'listen',
     description: '',
     aliases: [''],
     execute(bot, msg, args) {
-     msg.member.voice.channel.join();
+      bot.on("voiceStateUpdate", async (oldState, newState) => {
+      var server = newState.guild.id;
+        var isReady = true;
+      if (server == "612947002853949458") {
+          var member = newState.member
+          var voiceChannel = newState.channel;
+          const memberVoiceChannel = member.voice.channel
+            const connection = await memberVoiceChannel.join()
+            connection.on('speaking', (user, speaking) => {
+            if(voiceChannel != "undefined"){
+            if (speaking && isReady) {
+              console.log(`I'm listening to ${user.username}`)
+              isReady = false
+              sound("https://www.myinstants.com/media/sounds/great-success-borat.mp3", voiceChannel, msg)
+            } else {
+              isReady = true
+              console.log(`I stopped listening to ${user.username}`)
+            }
+            }
+            });
+      }})
+        msg.member.voice.channel.join().then(connection => {
+        const dispatcher = connection.play("https://cdn.glitch.com/05aa1396-7f5d-45a3-ab76-baf2815a144a%2Fuskyldig.mp3?v=1589662459594", { volume: 0});
+        dispatcher.destroy
+    }).catch(err => console.log(err));
+             }, 
+            };
+
+function sound(url, voiceChannel, msg) {
+  try{
+    if(voiceChannel != null){
+    voiceChannel.join().then(connection => {
+        const dispatcher = connection.play(url, { volume: 0.4});
+    }).catch(err => console.log(err));
+    }else msg.channel.send("Vet du forskjellen på voice og tekst? Gå inn i en voicechannel da din nepe");
+    }catch(err){
+      //console.log(err)
+    }}
+
+/*
+module.exports = {
+  name: 'listen',
+    description: '',
+    aliases: [''],
+    execute(bot, msg, args) {
       bot.on("voiceStateUpdate", async (oldState, newState) => {
       var server = newState.guild.id;
       if (server == "612947002853949458") {
@@ -15,12 +56,30 @@ module.exports = {
           const memberVoiceChannel = member.voice.channel
             const connection = await memberVoiceChannel.join()
             connection.on('speaking', (user, speaking) => {
+            if(voiceChannel != "undefined"){
             if (speaking) {
               console.log(`I'm listening to ${user.username}`)
+              sound("https://www.myinstants.com/media/sounds/great-success-borat.mp3", voiceChannel, msg)
             } else {
               console.log(`I stopped listening to ${user.username}`)
             }
+            }
             });
       }})
+        msg.member.voice.channel.join().then(connection => {
+        const dispatcher = connection.play("https://cdn.glitch.com/05aa1396-7f5d-45a3-ab76-baf2815a144a%2Fuskyldig.mp3?v=1589662459594", { volume: 0});
+        dispatcher.destroy
+    }).catch(err => console.log(err));
              }, 
             };
+
+function sound(url, voiceChannel, msg) {
+  try{
+    if(voiceChannel != null){
+    voiceChannel.join().then(connection => {
+        const dispatcher = connection.play(url, { volume: 0.4});
+    }).catch(err => console.log(err));
+    }else msg.channel.send("Vet du forskjellen på voice og tekst? Gå inn i en voicechannel da din nepe");
+    }catch(err){
+      //console.log(err)
+    }}*/
--- bot/cmds/stop.js
+++ bot/cmds/stop.js
@@ -4,12 +4,13 @@ module.exports = {
     aliases: [''],
     execute(bot, msg, args) {    
       try{
+        var voiceChannel = msg.member.voice.channel;
     if(voiceChannel != null){
     voiceChannel.join().then(connection => {
-        const dispatcher = connection.play(url, { volume: 0.4});
-        dispatcher.on("finish", end => voiceChannel.leave());
+        const dispatcher = connection.play("https://cdn.glitch.com/05aa1396-7f5d-45a3-ab76-baf2815a144a%2Fuskyldig.mp3?v=1589662459594", { volume: 0});
+        dispatcher.destroy
     }).catch(err => console.log(err));
     }else msg.channel.send("Vet du forskjellen på voice og tekst? Gå inn i en voicechannel da din nepe");
-    }catch(err){
+    }catch(err){}
     },
 };
