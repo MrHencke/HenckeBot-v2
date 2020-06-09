e2816adbebf20cc1ca1cd5774c821078316b84c8
Glitch (henckebot-test-branch) <none>
2020-06-09 18:52:34 +0000

    🏬🌗 Checkpoint ./bot/cmds/help.js:16220712/1970 ./bot/cmds/sounds/ban.js:16220712/41 ./bot/cmds/sounds/downs.js:16220712/15

--- bot/cmds/help.js
+++ bot/cmds/help.js
@@ -1,11 +1,32 @@
 module.exports = {
-    name: 'hjelp',
+    name: 'help',
     description: 'Du vet vel hva denne gjør?',
     category: 'hjelp',
     aliases: [''],
     execute(bot, msg, args) {
+     
+      var tekst = bot.commands.filter(cmd => cmd.category === "tekst")
+      var tekstOut
+      for(file of tekst){
+        
+      }
+      var nsfw = bot.commands.filter(cmd => cmd.category === "nsfw")
+      var nsfwOut
+      var youtube = bot.commands.filter(cmd => cmd.category === "youtube")
+      var youtubeOut
+      var tts = bot.commands.filter(cmd => cmd.category === "tts")
+      var ttsOut
+      var sound = bot.commands.filter(cmd => cmd.category === "sound")
+      var soundOut
+      var customsound = bot.commands.filter(cmd => cmd.category === "customsound")
+      var customsoundOut
+      var music = bot.commands.filter(cmd => cmd.category === "music")
+      var musicOut
       
+      var hjelpemetoder = bot.commands.filter(cmd => cmd.category === "hjelpemetoder")
+      var hjelpemetoderOut
       
+      console.log(tekst)
       
     },
 };
