9bf6fffb7d413fb8314b5b7d5e28cb59d1310907
Glitch (henckebot-test-branch) <none>
2020-06-09 13:23:33 +0000

    🏣👛 Checkpoint ./bot/cmds/c.js:16220712/1966

--- bot/cmds/c.js
+++ bot/cmds/c.js
@@ -5,6 +5,13 @@ const customPath = path.join(__dirname, '..', '..', 'res/lists/customClips.json'
 const customPathNyFil = require(path.join(__dirname, 'sounds/custom/'));
 const fs = require("fs")
 
+var nyPath = "require('path')"
+var nySound = "require(path.join(__dirname, '..', '..','..',  '..', 'res/sound.js'));"
+var module
+
+
+
+
 module.exports = {
     name: 'c',
     description: '',
@@ -12,7 +19,7 @@ module.exports = {
     execute(bot, msg, args) {
       
            if(args[0] == "list"){
-                var commandFiles = fs.readdirSync(path.join(__dirname, "/sounds/custom")).filter(file => file.endsWith(".js"));
+                var commandFiles = fs.readdirSync(path.join(__dirname, "sounds/custom")).filter(file => file.endsWith(".js"));
                 var customsounds = ""
                 for (const file of commandFiles) {
                 customsounds += file + ","
@@ -20,11 +27,40 @@ module.exports = {
              msg.channel.send(customsounds) 
                 } else if(args[0]== "add") {
                   var name = args[1]
-                  if(args[2].substring(0,4) == "http") var url = args[2];
-                  fs.readFile(customPath, 'utf8', function readFileCallback(err, data){
-                  if (err){
-                      console.log(err);
-                  } else {
+                  if(args[2].substring(0,4) == "http"){
+                    var url = args[2];
+                    fs.writeFile(customPathNyFil + name + ".js", nyPath + "\n" + nySound + "\n\n" + , (err) => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                    });
+                    
+                    
+                  } 
+                  
+                  
+                  
+                  }else {msg.channel.send("!c støtter bare list og add")} 
+               
+                    
+var path = require("path")
+const sound = require(path.join(__dirname, '..', '..','..',  '..', 'res/sound.js'));
+
+module.exports = {
+    name: 'placeholder',
+    description: '',
+  category: 'customsound',
+    aliases: [''],
+    execute(bot, msg, args) {
+  var url = "https://www.myinstants.com/media/sounds/hq-coffin-dance-funeral-vicetone-tony-igy-astronomia.mp3"
+                        sound(url,msg.member.voice.channel, msg);    },
+}; 
+      
+      
+      
+      
+      
+      
+                   /* 
                   var obj = JSON.parse(data);
                   console.log("Ny customsound addet av: " + msg.author.username)
                   obj.push({"name" : name, "url" : url, "bruker" : msg.author.username}); //add some data
@@ -37,7 +73,8 @@ module.exports = {
                   delete require.cache[require.resolve(path.join(__dirname, '..', '..', 'res/lists/'+file+".js"))]   // Deleting loaded module
                   custom = require(path.join(__dirname, '..', '..', 'res/lists/customClips.json'+file+".js"));
                   });}});
-                } else{msg.channel.send("!c støtter bare list og add")}   
+                  */
+                  
     },
 };
 
