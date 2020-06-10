231f595e50dc5c95d75a910bb1ab382242a68543
Glitch (henckebot-test-branch) <none>
2020-06-10 13:52:57 +0000

    🎊🌛 Checkpoint ./res/util/mail.js:16220712/739 ./bot/cmds/sendemail.js:16220712/567

--- bot/cmds/sendemail.js
+++ bot/cmds/sendemail.js
@@ -1,14 +1,25 @@
 var path = require("path")
-const mail = require(path.join(__dirname, '..', '..', 'res/util/mail.js'));
+const sendMail = require(path.join(__dirname, '..', '..', 'res/util/mail.js'));
 
 module.exports = {
     name: 'sendemail',
-    description: 'Sender en e-post til en gitt adresse, med gitt innhold',
+    description: 'Brukes ved !sendemail <e-post> <subject> <innhold>, Sender en e-post til en gitt adresse, med gitt innhold',
   category: 'tekst-personlig',
     aliases: ['email'],
     execute(bot, msg, args) {
           if (msg.author.id == process.env.HenckeID) {
-                  
+                  var toMail
+                  var subject
+                  var text
+                  console.log(args)    
+                  args.shift()
+                  console.log(args)    
+                  args.shift()
+                  console.log(args)    
+                  args.shift()
+                  console.log(args)    
+                  args.shift()
+                //sendMail(toMail, subject, text)
                 } else {
                   msg.channel.send("Hadde vært litt broken  å la deg bruke dette.");
                     return;
--- res/util/mail.js
+++ res/util/mail.js
@@ -1,8 +1,29 @@
-module.exports = function cmdSetup(bot, msg, toMail){
+const nodemailer = require("nodemailer")
+
+module.exports = function sendMail(toMail, subject, text){
   
-  var fromMail = "HenckeBot@gmail.com"
-   
+  var fromMail = "HenckeBot@outlook.com"
     
-    
+  const transporter = nodemailer.createTransport({
+    service: 'outlook',
+    auth: {
+        user: 'HenckeBot@outlook.com',
+        pass: process.env.mailPass
+    }
+});
   
+  let mailOptions = {
+    from: fromMail,
+    to: toMail,
+    subject: subject,
+    text: text
+};
+  
+  transporter.sendMail(mailOptions, (error, response) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log(response)
+});
+    
 }
