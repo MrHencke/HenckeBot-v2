002c09926f53e13581c5050f34b377a5ba0258c6
Glitch (henckebot-test-branch) <none>
2020-06-11 13:57:42 +0000

    📯🏠 Checkpoint ./bot/cmds/roastc.js:16220712/145

--- bot/cmds/roastc.js
+++ bot/cmds/roastc.js
@@ -4,11 +4,18 @@ const snakk = require(path.join(__dirname, "..", "..", "res/util/snakkiVoice.js"
 
 module.exports = {
   name: "roastc",
-  description: "Brukes ved !roastc @<person>, Roaster noen.",
+  description: "Brukes ved !roastc @<person>, Roaster noen, mer egne roasts.",
   category: 'tekst',
   aliases: ["svi"],
   execute(bot, msg, args) {
-    var mentioned = args[0];
+    var mentioned
+    const user = msg.mentions.users.first();
+  if (user === undefined) {
+    mentioned = args[0];
+  }else{
+    mentioned = user.username
+  }
+    
     var roast = mentioned + " " + roastc[Math.floor(Math.random() * roastc.length)]
     if(args[1] = null){
     msg.delete().catch(O_o => {});
