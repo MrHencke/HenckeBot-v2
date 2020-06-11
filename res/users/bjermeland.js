0cf5e2b2033cbdf324074cdd45c6b4b84bbd80be
Glitch (henckebot-test-branch) <none>
2020-06-11 17:42:08 +0000

    🚣🍎 Checkpoint

---
+++ res/users/bjermeland.js
@@ -0,0 +1,7 @@
+module.exports = {
+name: 'bjermeland',
+id: '294514795783258112',
+description: 'Customsound',
+toggled: true,
+url: 'https://retired.sounddogs.com/previews/2904/mp3/377351_SOUNDDOGS__fe.mp3',
+}; 
