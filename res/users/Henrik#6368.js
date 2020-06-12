b5d84bd57e04163ffa81839546b274842999420a
Glitch (henckebot-test-branch) <none>
2020-06-12 20:31:19 +0000

    🚛🌦 Checkpoint ./res/users/Henrik#6368.js:16220712/167

--- res/users/Henrik#6368.js
+++ res/users/Henrik#6368.js
@@ -1,13 +1,10 @@
-const discordTTS=require("discord-tts");
-const sang = "Vi seilte fra karibien og ankret opp i natt, Jeg har hørt et gammelt Rykte om en helt fantastisk skatt. Kaptein Sabeltann, er en farlig mann, Nå kan jeg lukte gull derfor ror vi inn mot land, tjo hei"
+//const discordTTS=require("discord-tts");
+//const sang = "Vi seilte fra karibien og ankret opp i natt, Jeg har hørt et gammelt Rykte om en helt fantastisk skatt. Kaptein Sabeltann, er en farlig mann, Nå kan jeg lukte gull derfor ror vi inn mot land, tjo hei"
 
 module.exports = {
 name: 'Henrik#6368',
 id: '133671473591222273',
 description: 'Customsound',
 toggled: true,
-url:  discordTTS.getVoiceStream(sang, "nb-NO")
-//url: 'https://www.myinstants.com/media/sounds/erro.mp3',
+url: 'https://www.myinstants.com/media/sounds/erro.mp3',
 };   
-console.log(typeof discordTTS.getVoiceStream(sang, "nb-NO"))
-console.log(discordTTS.getVoiceStream(sang, "nb-NO"))
