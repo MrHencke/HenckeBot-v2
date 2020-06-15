8fc8518f208dac64983a75dc80657901740ba378
Glitch (henckebot-test-branch) <none>
2020-06-15 11:48:30 +0000

    💫🖼 Checkpoint ./res/users/slothboi#2309.js:16220712/212

--- res/users/Henrik#6368.js
+++ res/users/Henrik#6368.js
@@ -1,10 +1,7 @@
-//const discordTTS=require("discord-tts");
-//const sang = "Vi seilte fra karibien og ankret opp i natt, Jeg har hørt et gammelt Rykte om en helt fantastisk skatt. Kaptein Sabeltann, er en farlig mann, Nå kan jeg lukte gull derfor ror vi inn mot land, tjo hei"
-
 module.exports = {
 name: 'Henrik#6368',
 id: '133671473591222273',
 description: 'Customsound',
 toggled: true,
-url: 'https://www.myinstants.com/media/sounds/erro.mp3',
-};   
+url: 'https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3',
+}; 
