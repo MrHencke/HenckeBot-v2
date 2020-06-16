993e4a1090f571acc0f5162fbc82fcf895473359
Glitch (henckebot-test-branch) <none>
2020-06-16 18:13:18 +0000

    💷🎅 Checkpoint ./bot/cmds/båt.js:16220712/1

--- bot/cmds/båt.js
+++ bot/cmds/båt.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'båt',
     description: 'Synger Boten Anna i TTS',
   category: 'tts',
-    aliases: [''],
+    aliases: [' '],
     execute(bot, msg, args) {
           msg.channel.send(" Jag känner en bot \n Hon heter Anna. Anna heter hon \n Och hon kan banna banna dig så hårt \n Hon röjer upp I våran kanal \n Jag vill berätta för dig att jag känner en bot",{ tts: true });
     },
